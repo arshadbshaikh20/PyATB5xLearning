0aec60c13fbdf75bd42fdcf0bd8575e8d5da5d88
arshad <arshadbshaikh20@gmail.com>
2024-11-11 08:24:35 +0530

    Task for User input Program

---
+++ Nov/ex_09112024_Litrals_variables/Lab029_Task_User_Input.py
@@ -0,0 +1,14 @@
+num1 = int(input("Enter the value"))
+num2 = int(input("Enter the value"))
+num3 = int(input("Enter the value"))
+print(type(num1))
+print(type(num2))
+print(type(num3))
+sum = num1 + num2 +num3
+sub = num1 - num2
+mul = num1 * num2 * num3
+div = num1 / num2 / num3
+print("Value of Sum is :", sum)
+print("Value of Sum is :", sub)
+print("Value of Sum is :", mul)
+print("Value of Sum is :", div)
---
+++ Nov/ex_09112024_Litrals_variables/Lab030_Task_User_Input_2.py
@@ -0,0 +1,8 @@
+num1 = float(input("Enter the value"))
+num2 = float(input("Enter the value"))
+print(type(num1))
+print(type(num2))
+quotient = num1 // num2
+reminder = num1 % num2
+print("Quotient is:", quotient)
+print("Reminder is:", reminder)
